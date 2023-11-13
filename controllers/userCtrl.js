32c1a1a3064f4048ff28969af73ba584449886a6
Sreerag PA <84066738+Sreeragpa@users.noreply.github.com>
2023-11-13 19:08:02 +0530

    Update userCtrl.js

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -9,7 +9,8 @@ exports.create = async (req,res)=>{
     const existingUser = await Userdb.findOne({email:req.body.email})
         
             if(existingUser){
-                res.send("User Already Exists")
+                // res.send("User Already Exists")
+                res.render("userlogin.ejs",{messages:{error:"User Already exists"}})
             }else{
                 try{
                     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -22,17 +23,38 @@ exports.create = async (req,res)=>{
     
                     await user.save() 
                         .then(()=>{
-                            res.send("User Added")
+                            res.redirect("/login")
                         })
                         .catch(err=>{
                             res.status(500).send(err)
                         })
                 }catch(err){
-                    console.log("erorooroor");
+                    res.status(500).send(err);
                 }
                 
              
             }
             
         
-}
+};
+
+exports.updatepass = async (req, res) => {
+    const email = req.body.email;
+    console.log(email);
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const updatedUser = await Userdb.findOneAndUpdate(
+            { email: email },
+            { $set: { password: hashedPassword } },
+            { new: true }
+        );
+
+        if (updatedUser) {
+            res.render('successpasschange.ejs');
+        }
+    } catch (err) {
+        console.error('Error updating password:', err);
+        res.status(500).send(err);
+    }
+};
