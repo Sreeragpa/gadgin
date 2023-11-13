39bee2c46ede29bc5692563a6f5d9bd484bb387d
Sreerag PA <84066738+Sreeragpa@users.noreply.github.com>
2023-11-13 19:08:23 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,36 @@
 const route = require('express').Router();
 const userCtrl=require('../controllers/userCtrl')
 const otpCtrl=require('../controllers/otpCtrl')
-const {CheckAuthenticated} = require('../middlewares/authMiddleware')
+const session = require('express-session')
+const {CheckAuthenticated,CheckNotauthenticated} = require('../middlewares/authMiddleware')
 route.get('/',CheckAuthenticated,(req,res)=>{
-    res.send("Homepage")
+    res.render("homepage.ejs")
+    
 })
-route.get('/login',(req,res)=>{
+route.get('/login',CheckNotauthenticated,(req,res)=>{
+  
     res.render("userlogin.ejs")
 })
-route.get('/register',(req,res)=>{
+route.get('/register',CheckNotauthenticated,(req,res)=>{
     res.render("emailreg.ejs")
 })
+route.get('/forgotpass',(req,res)=>{
+    res.render('forgotpass.ejs')
+})
+route.post('/logout',(req,res)=>{
+    // console.log(req.session);
+    userId=req.session.passport;
+        req.session.destroy(err=>{
+        if(err){
+            res.send(err);
+        }else{
+            res.redirect("/login")
+        }
+    });
+    
+})
 
+route.post('/api/update',userCtrl.updatepass)
 route.post('/api/register',userCtrl.create)
 route.post('/api/getotp',otpCtrl.getOtp)
 route.post('/api/checkotp',otpCtrl.checkOtp)
